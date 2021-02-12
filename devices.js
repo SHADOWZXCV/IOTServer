2b700a0a237fd631e3b5746b012d6e030acf415b
SHADOWZXCV <faded.lover.shadows@gmail.com>
2021-02-12 20:05:25 +0200

    master

--- devices.js
+++ devices.js
@@ -1,13 +1,9 @@
 const mqtt = require('mqtt');
 exports.connectMQTT = connectMQTT;
 
-const host = 'mqtt://test.mosquitto.org:1883'
-exports.mqtt_s = host;
 
-function connectMQTT(subscribe = 0, message = '', topic = '') {
+function connectMQTT(client, subscribe = 0, message = '', topic = '') {
     const clientId = 'mqttjs_' + Math.random().toString(16).substr(2, 8)
-    console.log('Connecting mqtt client')
-    const client = mqtt.connect(host)
 
     client.on('error', (err) => {
         console.log('Connection error: ', err)
@@ -19,11 +15,9 @@ function connectMQTT(subscribe = 0, message = '', topic = '') {
     })
 
     if (subscribe != 0 && message != '' && topic != '') {
-        client.on('connect', () => {
-            console.log('Client connected:' + clientId)
-            // Publish
-                client.publish(topic, message)
-        })
+        console.log('Client connected:' + clientId)
+        // Publish
+        client.publish(topic, message)
+
     }
 }
-
--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // And mqtt for fast, easy IoT-to-server data transmittion.
 // Current database cluster is free atm, holds up to 500 connections, available at: https://cloud.mongodb.com/v2/5ffb1ceb9467e10b4446522f#clusters/edit/ClusterIOT.
 // required made-modules
-// const MQTT_C = require('./devices');
+const MQTT_C = require('./devices');
 // required modules
 const app = require('express')();
 const server = require('http').Server(app);
@@ -11,17 +11,17 @@ const io = require('socket.io')(server);
 const {
     MongoClient
 } = require('mongodb');
-// const mqtt = require('mqtt');
+const mqtt = require('mqtt');
 ///////////////////////
 
-// const client = mqtt.connect('mqtt://test.mosquitto.org:1883');
+const clientMQTT = mqtt.connect('mqtt://test.mosquitto.org:1883');
 
-// client.subscribe('sus', function (err) {
-//     if (err) console.log(err);
-// })
-// client.on('message', function (topic, message) {
-//     console.log(topic + ' s ' + message)
-// });
+clientMQTT.subscribe('sus', function (err) {
+    if (err) console.log(err);
+})
+clientMQTT.on('message', function (topic, message) {
+    console.log(topic + ' s ' + message)
+});
 ///////////////////
 
 // setup server
@@ -62,7 +62,7 @@ io.on('connection', socket => {
     console.log('connected socket to server ' + socket.id);
     // changes listening
     socket.on('changs', data => {
-        console.log('emitting' + data);
+        console.log('emitting' + data.Status);
         try {
             const devices = client.db('IOT').collection('Devices');
             devices.findOne({
@@ -76,7 +76,7 @@ io.on('connection', socket => {
                 } else {
                     console.log('found it');
                     // note: 1 is for publishing!
-                    // MQTT_C.connectMQTT(1, data.Status, 'sus');
+                    MQTT_C.connectMQTT(clientMQTT, 1, data.Status, 'sus');
                     devices.updateOne({
                         "ID": data.DeviceID
                     }, {
