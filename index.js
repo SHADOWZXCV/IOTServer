99ae06a4b48110e56fefd557ba3281f14f5f3689
SHADOWZXCV <faded.lover.shadows@gmail.com>
2021-02-12 16:51:16 +0200

    commit on 'master'

--- index.js
+++ index.js
@@ -3,24 +3,24 @@
 // And mqtt for fast, easy IoT-to-server data transmittion.
 // Current database cluster is free atm, holds up to 500 connections, available at: https://cloud.mongodb.com/v2/5ffb1ceb9467e10b4446522f#clusters/edit/ClusterIOT.
 // required made-modules
-const MQTT_C = require('./devices');
+// const MQTT_C = require('./devices');
 // required modules
 const express = require('express');
 const app = express();
 const http = require('http');
 const socket = require('socket.io');
 const mongoDB = require('mongodb');
-const mqtt = require('mqtt');
+// const mqtt = require('mqtt');
 ///////////////////////
 
-const client = mqtt.connect('mqtt://test.mosquitto.org:1883');
+// const client = mqtt.connect('mqtt://test.mosquitto.org:1883');
 
-client.subscribe('sus', function (err) {
-    if (err) console.log(err);
-})
-client.on('message', function (topic, message) {
-    console.log(topic + ' s ' + message)
-});
+// client.subscribe('sus', function (err) {
+//     if (err) console.log(err);
+// })
+// client.on('message', function (topic, message) {
+//     console.log(topic + ' s ' + message)
+// });
 ///////////////////
 
 // setup server
@@ -38,49 +38,49 @@ const io = socket(server);
 
 
 app.get('/', function (req, res, next) {
-io.on('connection', socket => {
-    console.log('connected socket to server ' + socket.id);
-    mongoDB.connect(URL, function (err, db) {
-        if (err) throw err;
-        const table = db.db("IOT");
-        const devices = table.collection('Devices');
+    io.on('connection', socket => {
+        console.log('connected socket to server ' + socket.id);
+        mongoDB.connect(URL, function (err, db) {
+            if (err) throw err;
+            const table = db.db("IOT");
+            const devices = table.collection('Devices');
 
-        // changes listening
-        socket.on('changing', data => {
-            console.log('emitted!');
-            devices.findOne({
-                "ID": data.DeviceID
-            }, function (err, res) {
-                if (!res) {
-                    console.log('Nothing');
-                    db.close();
-                } else if (err) {
-                    console.log(err);
-                } else {
-                    // note: 1 is for publishing!
-                    MQTT_C.connectMQTT(1, data.Status, 'sus');
-                    devices.updateOne({
-                        "ID": data.DeviceID
-                    }, {
-                        $set: {
-                            "Status": data.Status
-                        }
-                    }).then(function (res) {
-                        if (res) {
-                            socket.emit('changing', {
-                                Result: 1
-                            });
-                        }
-                    });
+            // changes listening
+            socket.on('changing', data => {
+                console.log('emitted!');
+                devices.findOne({
+                    "ID": data.DeviceID
+                }, function (err, res) {
+                    if (!res) {
+                        console.log('Nothing');
+                        db.close();
+                    } else if (err) {
+                        console.log(err);
+                    } else {
+                        // note: 1 is for publishing!
+                        // MQTT_C.connectMQTT(1, data.Status, 'sus');
+                        devices.updateOne({
+                            "ID": data.DeviceID
+                        }, {
+                            $set: {
+                                "Status": data.Status
+                            }
+                        }).then(function (res) {
+                            if (res) {
+                                socket.emit('changing', {
+                                    Result: 1
+                                });
+                            }
+                        });
 
-                }
+                    }
 
+                });
             });
         });
     });
-});
 
-    
+
 });
 
 
