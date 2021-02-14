56ebb7fb4c8742358cc312037a605c1fb974a506
SHADOWZXCV <faded.lover.shadows@gmail.com>
2021-02-14 12:09:59 +0200

    master

--- devices.js
+++ devices.js
@@ -1,6 +1,6 @@
 const mqtt = require('mqtt');
 exports.connectMQTT = connectMQTT;
-
+exports.findDevice = findDeviceInDB;
 
 function connectMQTT(client, subscribe = 0, message = '', topic = '') {
     const clientId = 'mqttjs_' + Math.random().toString(16).substr(2, 8)
@@ -20,4 +20,27 @@ function connectMQTT(client, subscribe = 0, message = '', topic = '') {
         client.publish(topic, message)
 
     }
+}
+
+function findDeviceInDB(data, clientMongo) {
+    const devices = clientMongo.db('IOT').collection('Devices');
+    devices.findOne({
+        "ID": data.DeviceID
+    }, function (err, res) {
+        if (!res) {
+            console.log('Nothing');
+            db.close().then((data) => console.log('db closed'));
+        } else if (err) {
+            console.log(err);
+        } else {
+            console.log('found it');
+            devices.updateOne({
+                "ID": data.DeviceID
+            }, {
+                $set: {
+                    "Status": data.Status
+                }
+            })
+        }
+    });
 }
