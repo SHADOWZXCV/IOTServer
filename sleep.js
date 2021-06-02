6e6f629eda4e8f76dfa8822e0a59f57e82027f0e
Alex <34347098+SHADOWZXCV@users.noreply.github.com>
2021-06-02 22:24:04 +0200

    Update sleep.js

--- sleep.js
+++ sleep.js
@@ -1,7 +1,3 @@
-async function waiting_for_response(delay){
-    const sleep = (delay)=> new Promise((resolve)=> setTimeout(resolve,delay))
-     await sleep(delay);
+const sleep = (delay)=> new Promise((resolve)=> setTimeout(resolve,delay))
 
-}
-
-module.exports = waiting_for_response
+module.exports = sleep
